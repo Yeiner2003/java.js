c8dcd483289b51c10ae1e10f98e3b017b6b4eac4
Yeiner2003 <148366001+Yeiner2003@users.noreply.github.com>
2024-04-29 11:43:58 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,30 @@
+//#1
+//let nombre = prompt ("por favor ingrese su nombre");
+
+//let edad = prompt (" por favor ingrese su edad");
+
+//parseInt (edad);
+
+//edad <=17?alert(`su nombre es: ${nombre} y es menor de edad : ${edad}`): alert(`su nombre es : ${nombre}: es mayor de edad y ${edad}`);
+
+//#2
+//let A = parseInt(prompt("Ingrese el primer valor:"));
+
+//let B = parseInt(prompt("Ingrese el segundo valor:"));
+//let mayor = (A > B) ? A : B; 
+
+//alert(`El mayor valor es:` + mayor); 
+//if (A === B) {
+  // alert(`Error Los valores ingresados son iguales Por favor ingrese valores distintos.`);
+    // } else {
+   
+//}
+//if(condicion){
+//    mensaje
+//}else if(condicion){
+  //  mensaje
+//}else{
+  //  mensaje
+//}
+
+
